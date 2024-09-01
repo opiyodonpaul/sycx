d66fca2c61aef5ef0c81cf5ec33ded5aaeb47eaa
Don Artkins <opiyodon9@gmail.com>
2024-09-01 22:55:02 +0300

    changed debug mode to false in app.py for production purposes

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def download_summary():
     return send_file(buffer, mimetype=mime_type, as_attachment=True, download_name=f'summary.{file_format}')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
