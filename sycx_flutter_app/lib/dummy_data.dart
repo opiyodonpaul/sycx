8e874d0bef2c3b23cf570cbf8f9d8c57cfab265e
Don Artkins <opiyodon9@gmail.com>
2024-09-09 03:39:28 +0300

    code update

--- sycx_flutter_app/lib/dummy_data.dart
+++ sycx_flutter_app/lib/dummy_data.dart
@@ -54,25 +54,21 @@ class DummyData {
       'name': 'Research Paper.pdf',
       'size': '2.5 MB',
       'type': 'PDF',
-      'icon': 'assets/icons/pdf_icon.png',
     },
     {
       'name': 'Presentation Slides.pptx',
       'size': '5.1 MB',
       'type': 'PPTX',
-      'icon': 'assets/icons/ppt_icon.png',
     },
     {
       'name': 'Data Analysis.xlsx',
       'size': '1.8 MB',
       'type': 'XLSX',
-      'icon': 'assets/icons/excel_icon.png',
     },
     {
       'name': 'Project Report.docx',
       'size': '3.2 MB',
       'type': 'DOCX',
-      'icon': 'assets/icons/word_icon.png',
     },
   ];
 }
